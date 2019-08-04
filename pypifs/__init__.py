45b7815131e4305359d4cea9ba3e031c877cd8a5
chfw <wangc_2011@hotmail.com>
2019-08-04 19:30:02 +0100

    :books: configure the tests dependency and package dependencies

--- pypifs/__init__.py
+++ pypifs/__init__.py
@@ -1,3 +1,6 @@
 # flake8: noqa
 from pypifs._version import __version__
 from pypifs._version import __author__
+
+from pypifs.opener import PypiFSOpener
+
--- pypifs/opener.py
+++ pypifs/opener.py
@@ -11,8 +11,8 @@ class PypiFSOpener(Opener):
 
     def open_fs(self, fs_url, parse_result, writeable, create, cwd):
         pypi_package_name, _, dir_path = parse_result.resource.partition("/")
-        module = __import__(pypi_package_name)
-        directory = os.path.dirname(sys.modules[module].__file__)
+        __import__(pypi_package_name)
+        directory = os.path.dirname(sys.modules[pypi_package_name].__file__)
         directory = os.path.normcase(directory)
         root_path = fs.path.join(directory, dir_path)
         osfs = OSFS(root_path=root_path)
